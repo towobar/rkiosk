bd7726b81a72bc4ffba4e48226d67ade6ff7d6b6
towobar <plux@freenet.de>
2016-04-15 11:30:32 +0200

    change welcome to !!

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,7 +11,7 @@
     <div class="well service text-justify">
 
        <p>
-            Liebe Haselhorster ##, <br><br>
+            Liebe Haselhorster !!, <br><br>
 
             hier findet Ihr ein Überblick unseres Angebotes.
             Um unseren Service zu optimieren, habt Ihr hier die Möglichkeit Artikel vorzubestellen,
