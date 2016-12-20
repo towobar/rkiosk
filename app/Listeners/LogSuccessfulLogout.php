202389863c480941c05b19991501dcdb4941845a
towobar <plux@freenet.de>
2016-12-20 11:11:13 +0100

    Implemented : Delete Cart when user logout,

---
+++ app/Listeners/LogSuccessfulLogout.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Auth\Events\Logout;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Gloudemans\Shoppingcart\Facades\Cart;
+
+class LogSuccessfulLogout
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  Logout  $event
+     * @return void
+     */
+    public function handle(Logout $event)
+    {
+
+        // Beim logout den Wahrenkorb des users löschen
+        Cart::instance('shopping')->destroy();
+    }
+}
